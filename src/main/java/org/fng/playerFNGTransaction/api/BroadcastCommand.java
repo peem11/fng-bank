87f820ce95a56eed4681f9a32dd2fd915044439c
samuelb30 <2351016@csfoy.ca>
2025-04-23 20:32:06 -0400

    Ajout de la commande Broadcast

---
+++ src/main/java/org/fng/playerFNGTransaction/api/BroadcastCommand.java
@@ -0,0 +1,38 @@
+package org.fng.playerFNGTransaction.api;
+
+import net.luckperms.api.LuckPerms;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.fng.playerFNGTransaction.PlayerFNGTransaction;
+import org.jetbrains.annotations.NotNull;
+import java.util.List;
+import java.util.Objects;
+
+
+
+public class BroadcastCommand implements CommandExecutor {
+
+    public BroadcastCommand(){
+      
+    }
+    @Override
+    public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+
+        List<Player> players = Bukkit.getOnlinePlayers().stream().map(Player::getPlayer).toList();
+        if(args.length > 1){
+            players = players.stream().filter(player -> player.hasPermission("group." + args[0])).toList();
+            players.forEach(player -> {
+                player.sendMessage(PlayerFNGTransaction.PLUGIN_PREFIX + args[1]);
+            });
+            return true;
+        }
+        players.forEach(player -> {
+            player.sendMessage(PlayerFNGTransaction.PLUGIN_PREFIX + args[0]);
+        });
+
+        return true;
+    }
+}
